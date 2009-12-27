24d976cd5c05e59524f515bb65aa73ae19ca83d6
Simon Urbanek <simon.urbanek@R-project.org>
2009-12-27 04:24:31 +0000

    add gc.c for eventual GC code
    git-svn-id: https://svn.rforge.net/aleph/trunk@10 c44be17a-7480-4b1b-a17a-88d156aacfc2

---
+++ gc.c
@@ -0,0 +1,5 @@
+#include "aleph.h"
+
+/* this will eventually perform the garbage collection. It is callen by low-level memory alloc functions when they're running out of memory -- should probably be called more often, though ... */
+void gc_run(size_t needed) {
+}
